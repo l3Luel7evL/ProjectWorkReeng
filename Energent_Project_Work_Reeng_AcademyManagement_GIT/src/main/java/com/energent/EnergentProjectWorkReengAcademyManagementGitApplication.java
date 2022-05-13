f07cf8ebb4f8ed292223fa9a5e62b6f3d7b3e80f
UTENTE <UTENTE@192.168.102.239>
2022-05-13 09:42:01 +0200

    v.0 reeng

---
+++ Energent_Project_Work_Reeng_AcademyManagement_GIT/src/main/java/com/energent/EnergentProjectWorkReengAcademyManagementGitApplication.java
@@ -0,0 +1,13 @@
+package com.energent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergentProjectWorkReengAcademyManagementGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergentProjectWorkReengAcademyManagementGitApplication.class, args);
+	}
+
+}
